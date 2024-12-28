d2b29e00fb963656fd399089ef58c89edb193171
= <=>
2024-12-28 15:54:04 -0500

    add example for csv to string[]

--- render.py
+++ render.py
@@ -1,16 +1,24 @@
 from jinja2 import Template, Environment, StrictUndefined
 
+def split_filter(value, delimiter=","):
+    return value.split(delimiter)
+
+env = Environment(
+    block_start_string='{%',
+    block_end_string='%}',
+    variable_start_string='#{',
+    variable_end_string='}',
+    undefined=StrictUndefined
+)
+env.filters['split'] = split_filter
+
 def recursive_resolve_context(context):
     def resolve(value, context):
         if isinstance(value, str):
             prev_value = None
             while value != prev_value:
                 prev_value = value
-                value = Environment(
-                    variable_start_string='#{',
-                    variable_end_string='}',
-                    # undefined=StrictUndefined
-                ).from_string(value).render(context)
+                value = env.from_string(value).render(context)
             return value
         elif isinstance(value, dict):
             return {k: resolve(v, context) for k, v in value.items()}
@@ -23,6 +31,7 @@ def recursive_resolve_context(context):
 context = {
     "servername": "https://api.foo.com",
     "apis": ["https://#{projectname | lower()}.#{baseurl}", "https://api2.foo.com", "https://api3.foo.com"],
+    "apiscsv": "https://#{projectname | lower()}.#{baseurl}, https://api2.foo.com, https://api3.foo.com",
     "baseurl": "foo.com",
     "projectname": "API1"
 }
@@ -31,19 +40,12 @@ json_template_string = '''
 {
   "servername": "#{ servername }",
   "apis": [ "#{apis | join(", ")}" ],
+  "apis-2": [ "#{apiscsv | split(',') | map('trim') | join('", "')}" ],
   "options": {
     "a": "{b}"
   }
 }
 '''
-
-env = Environment(
-    block_start_string='{%',
-    block_end_string='%}',
-    variable_start_string='#{',
-    variable_end_string='}',
-    undefined=StrictUndefined
-)
 
 template = env.from_string(json_template_string)
 
